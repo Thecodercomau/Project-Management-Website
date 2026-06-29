5d5f3c0b2527fc1c2f085ff35a683e191c5b3be4
StudentRoahanfromCodingal <roahansenthilkumar0034257@gmail.com>
2026-06-30 07:21:03 +0800

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+    "https://ihcyxthkdmqlkxsfwyoo.supabase.co",
+    "sb_publishable_vgEiWPlIVHmJGQ-nQoPA9w_vnqvZJIC"
+);
